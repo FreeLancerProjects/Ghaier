95b0b7a8bfcc3dc63f9fd9985e3cc4a356b2d6a5
End Point <ahmedmohamed23345@gmail.com>
2020-07-12 10:07:05 +0200

    ch

---
+++ app/src/main/java/com/ghiar/activities_fragments/activity_service_center_detials/ServiceCenterDetialsActivity.java
@@ -0,0 +1,78 @@
+package com.ghiar.activities_fragments.activity_service_center_detials;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.ghiar.R;
+import com.ghiar.databinding.ActivityServicesCenterBinding;
+import com.ghiar.databinding.ActivityServicesCenterDetialsBinding;
+import com.ghiar.interfaces.Listeners;
+import com.ghiar.language.Language;
+import com.ghiar.models.UserModel;
+import com.ghiar.preferences.Preferences;
+
+import java.util.Locale;
+
+import io.paperdb.Paper;
+
+public class ServiceCenterDetialsActivity extends AppCompatActivity implements Listeners.BackListener {
+    private ActivityServicesCenterDetialsBinding binding;
+    private String lang;
+    // private List<NotificationDataModel.NotificationModel> notificationModelList;
+    //private NotificationAdapter adapter;
+    private Preferences preferences;
+    private UserModel userModel;
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        Paper.init(newBase);
+        super.attachBaseContext(Language.updateResources(newBase, Paper.book().read("lang", "ar")));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_services_center_detials);
+        initView();
+    }
+
+
+    private void initView() {
+        Paper.init(this);
+        lang = Paper.book().read("lang", Locale.getDefault().getLanguage());
+        binding.setBackListener(this);
+        binding.setLang(lang);
+        binding.progBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+
+        preferences = Preferences.getInstance();
+        userModel = preferences.getUserData(this);
+        Paper.init(this);
+        lang = Paper.book().read("lang", Locale.getDefault().getLanguage());
+        binding.setBackListener(this);
+        binding.setLang(lang);
+        binding.progBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        binding.recView.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+
+    @Override
+    public void back() {
+//
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        back();
+    }
+
+
+}
