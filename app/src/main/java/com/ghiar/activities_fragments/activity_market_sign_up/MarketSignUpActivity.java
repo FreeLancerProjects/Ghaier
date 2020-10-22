4996ecf58b3e90c7989af2111836a77b54100336
End Point <ahmedmohamed23345@gmail.com>
2020-10-22 17:13:49 +0200

    ch

---
+++ app/src/main/java/com/ghiar/activities_fragments/activity_market_sign_up/MarketSignUpActivity.java
@@ -0,0 +1,25 @@
+package com.ghiar.activities_fragments.activity_market_sign_up;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ghiar.R;
+import com.ghiar.tags.Tags;
+
+public class MarketSignUpActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_as_wakeel);
+
+        mWebView = findViewById(R.id.webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl(Tags.base_url+"admin/market-register-form");
+    }
+}
