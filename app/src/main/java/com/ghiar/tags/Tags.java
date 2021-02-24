a48f767c40e666ee6a7001f2e3f3de73648688c7
soft <emadmagdy.developer@gmail.com>
2021-02-24 13:46:25 +0200

    emad

--- app/src/main/java/com/ghiar/tags/Tags.java
+++ app/src/main/java/com/ghiar/tags/Tags.java
@@ -3,10 +3,10 @@ package com.ghiar.tags;
 import android.os.Environment;
 
 public class Tags {
-  //https://documenter.getpostman.com/view/11297871/TVCfXUZx#21b567d3-be41-4d7c-bf43-1d468df00728
-    public static String base_url = "http://ghair.creativeshare.sa/";
-  public static final String googleDirectionBase_url = "https://maps.googleapis.com/maps/api/";
-  public static final String IMAGE_URL= base_url;
+    //https://documenter.getpostman.com/view/11297871/TVCfXUZx#21b567d3-be41-4d7c-bf43-1d468df00728
+    public static String base_url = "http://gahier.com/";
+    public static final String googleDirectionBase_url = "https://maps.googleapis.com/maps/api/";
+    public static final String IMAGE_URL = base_url;
     public static final String session_login = "login";
     public static final String session_logout = "logout";
     public static final String not_tag = "ghaier_not_tag";
